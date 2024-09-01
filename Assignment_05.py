588cd6ccbb767056af365655cc8f9655b0a8a01c
KashafNaveedAhmad <kashafnaveed329@gmail.com>
2024-09-02 00:34:45 +0500

    EVEN/ODD

--- Assignment_05.py
+++ Assignment_05.py
@@ -1,2 +1,18 @@
 # Assignment-05
-print("Let's start the Assignment")
+print("\nLet's start the Assignment!!!")
+
+
+#Even or Odd
+print("\n->Check for Even or Odd\n")
+
+try:
+    number_input=int(input("Enter the value: "))
+    if number_input %2==0 :
+        print(f"Number entered '{number_input}' is even!")
+    else:
+        print(f"Number entered '{number_input}'is odd!")
+except ValueError:
+    print("Please enter a valid response!")
+print("\n-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_")
+
+print("THANKYOU!!")  
