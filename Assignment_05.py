771cc710b1827145ff4ccc629ad80cdcbec69d04
KashafNaveedAhmad <kashafnaveed329@gmail.com>
2024-09-04 21:47:42 +0500

    added code

--- Assignment_05.py
+++ Assignment_05.py
@@ -15,4 +15,38 @@ except ValueError:
     print("Please enter a valid response!")
 print("\n-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_")
 
+#Positive, Negative, or Zero.
+print("\n->Check for Positive, Negative, or Zero.\n")
+
+try:
+    number_input=int(input("Enter the value: "))
+    if number_input<0 :
+        print(f"'{number_input}'is negative!")
+    elif(number_input>0):
+        print(f"'{number_input}' is positive!")
+    else:
+        print(f"'{number_input}' is zero!")
+except ValueError:
+    print("Please enter a valid response!")
+print("\n-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_")
+
+
+#Whether it is divisible by both 2 and 3 or anyone of them or not divisible by both check all the cases and print statement for each case.
+print("\n->Checking for conditions regarding '2' and'3'")
+
+try:
+    number_input=int(input("\nEnter the value: "))
+    if number_input%3==0 :
+        print(f"'{number_input}'is divisible by 3!")
+    elif(number_input%2==0):
+        print(f"'{number_input}' is divisible by 2!")
+    elif((number_input%2==0)and(number_input%3==0)):
+        print(f"'{number_input}' is divisible by both '2' and '3'!")
+    else:
+        print(f"'{number_input}' is not divisible by both '2' and '3'!")
+except ValueError:
+    print("Please enter a valid response!")
+print("\n-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_")
+
+
 print("THANKYOU!!")  
